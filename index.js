e8a1d270d0d5a291cfdf13f4469b86c875fd0371
Thegustzazaa <s6506021622167@email.kmutnb.ac.th>
2024-02-12 11:17:41 +0700

    up

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const express = require('express')
 const axios = require('axios')
-const path = require('path')
+    //const path = require('path')
 const app = express()
 var bodyParser = require('body-parser')
 
 
 
-const base_url = "http://node56409-pawarit.proen.app.ruk-com.cloud"
+const base_url = "node56413-pawarit.proen.app.ruk-com.cloud:11550"
 
-app.set('views', path.join(__dirname, "/public/views"))
+//app.set('views', path.join(__dirname, "/public/views"))
 app.set('view engine', 'ejs')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
